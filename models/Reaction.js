b8ae0a4761aeee54446057fe2b783711fb0c65eb
unknown <joseph.harley02@gmail.com>
2023-02-24 20:34:18 -0500

    created thought model and reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const { Schema } = require("mongoose");
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: new Schema.Types.ObjectId
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: `${Date.prototype.getMonth()}, ${Date.prototype.getDay()}, ${Date.prototype.getFullYear()}`
+        }
+    }
+);
+
+module.exports = reactionSchema;
