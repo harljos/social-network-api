b6c3052db85c2d19f450eed0c33367e46ba0b3d0
unknown <joseph.harley02@gmail.com>
2023-02-25 15:32:00 -0500

    created api routes for adding and removing a friend from the user's friend list

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,7 +30,7 @@ module.exports = {
     // Updates a user by id
     updateUser (req, res) {
         User.findOneAndUpdate(
-            { _id: req.params.courseId },
+            { _id: req.params.userId },
             { $set: req.body },
             { runValidators: true, new: true }
         )
@@ -49,10 +49,40 @@ module.exports = {
         User.findOneAndDelete({ _id: req.params.userId })
             .then((user) => {
                 if (!user) {
-                    res.status(404).json({ message: "No user with the ID" })
+                    res.status(404).json({ message: "No user with that ID" })
                 }
             })
             .then(() => res.status(200).json({ message: "User has been deleted" }))
             .catch((err) => res.status(500).json(err));
+    },
+    // Adds a new friend to a user's friend list
+    addFriend (req, res) {
+        User.findOneAndUpdate(
+            { _id: req.body.userId},
+            { $push: { friends: req.body.friendId}},
+            { new: true }
+        )
+        .then((user) => {
+            if (!user) {
+                res.status(404).json({ message: "No user with that ID" });
+            }
+        })
+        .then(() => res.status(200).json({ message: "friend added" }))
+        .catch((err) => res.status(500).json(err));
+    },
+    // Deletes friend from user's friend list
+    deleteFriend (req, res) {
+        User.findOneAndUpdate(
+            { _id: req.body.userId},
+            { $pull: { friends: req.body.friendId}},
+            { new: true }
+        )
+        .then((user) => {
+            if (!user) {
+                res.status(404).json({ message: "No user with that ID" });
+            }
+        })
+        .then(() => res.status(200).json({ message: "friend removed" }))
+        .catch((err) => res.status(500).json(err));
     }
 };
