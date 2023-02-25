33a8e5f4a4976f4f41a3a9cdf4c8351475cd5cee
unknown <joseph.harley02@gmail.com>
2023-02-25 14:09:29 -0500

    added routes for getting a single user, creating a user, updating a user, and deleting a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,4 +7,52 @@ module.exports = {
             .then((users) => res.status(200).json(users))
             .catch((err) => res.status(500).json(err));
     },
+    // Gets a single user by id
+    getSingleUser (req, res) {
+        User.findOne({ _id: req.params.userId })
+            .select("-__v")
+            .then((user) => {
+                if (!user) {
+                    res.status(404).json({ message: "No user with that ID" })
+                }
+                else {
+                    res.status(200).json(user)
+                }
+            })
+            .catch((err) => res.status(500).json(err));
+    },
+    // Create new user
+    createUser (req, res) {
+        User.create(req.body)
+            .then((user) => res.status(200).json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+    // Updates a user by id
+    updateUser (req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.courseId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .then((user) => {
+            if (!course) {
+                res.status(404).json({ message: "No user with that ID "});
+            }
+            else {
+                res.status(200).json(user);
+            }
+        })
+        .catch((err) => res.status(500).json(err));
+    },
+    // Deletes user by id
+    deleteUser (req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) => {
+                if (!user) {
+                    res.status(404).json({ message: "No user with the ID" })
+                }
+            })
+            .then(() => res.status(200).json({ message: "User has been deleted" }))
+            .catch((err) => res.status(500).json(err));
+    }
 };
